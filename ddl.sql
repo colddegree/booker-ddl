d63ae48882dc24fc9c90791137cad6fff257c86c
Alexander Melnik <colddegree@yandex.ru>
2020-06-09 01:33:50 +1000

    Добавить/поправить кучу таблиц (осталось доделать заказ и услуги)

--- ddl.sql
+++ ddl.sql
@@ -1,27 +1,34 @@
-drop table if exists room_type_facility_bind;
-drop table if exists room_type_bed_bind;
-drop table if exists hotel_payment_type_bind;
-drop table if exists hotel_photo_bind;
-drop table if exists payment_type;
-drop table if exists payment_log;
-drop table if exists `order`;
-drop table if exists payment_status;
-drop table if exists user;
-drop table if exists photo;
-drop table if exists bed;
-drop table if exists facility;
-drop table if exists room_status;
-drop table if exists room;
-drop table if exists room_type_discount;
-drop table if exists room_type;
-drop table if exists hotel;
-drop table if exists locality;
+drop database if exists booker;
+create database booker character set utf8mb4 collate utf8mb4_unicode_ci;
+use booker;
 
 # TODO: добавить хранение соглашения с версионностью и соглашение с правилами гостиницы в заказ
 
-create table locality ( # населённый пункт
+create table locality ( # местоположение
   id serial,
   name varchar(255) not null
+);
+
+create table user ( # пользователь
+  id serial,
+  type tinyint unsigned not null, # 1 - менеджер, 2 - клиент
+  login varchar(255) not null, # TODO: добавить уникальность поля login
+  hashed_password varchar(255) not null,
+  name varchar(255) not null,
+  phone_number varchar(255),
+  email_address varchar(255)
+);
+
+create table user_manager ( # менеджер
+  id serial,
+  user_id bigint unsigned not null,
+  foreign key (user_id) references user(id)
+);
+
+create table user_customer ( # клиент
+  id serial,
+  user_id bigint unsigned not null,
+  foreign key (user_id) references user(id)
 );
 
 create table hotel ( # гостиница
@@ -29,32 +36,52 @@ create table hotel ( # гостиница
   name varchar(255) not null,
   description text,
   locality_id bigint unsigned not null,
-  address varchar(255) not null,
-  contact_info text,
-  distance_to_locality_center_in_kilometers float,
-  foreign key (locality_id) references locality(id)
+  stars_count tinyint unsigned not null default 0,
+  address varchar(255) not null unique,
+  phone_number varchar(255) not null,
+  email_address varchar(255) not null,
+  manager_id bigint unsigned not null unique,
+  foreign key (locality_id) references locality(id),
+  foreign key (manager_id) references user_manager(id)
 );
 
-create table room_type ( # тип гостиничного номера
+create table hotel_attraction ( # важное место (достопримечательность)
   id serial,
-  hotel_id bigint unsigned not null,
   name varchar(255) not null,
-  price_per_day_in_rubles int unsigned not null,
+  hotel_id bigint unsigned not null,
   foreign key (hotel_id) references hotel(id)
 );
 
-create table room_type_discount ( # скидка для типа гостиничного номера на конкретную дату
+create table hotel_rule ( # правило гостиницы
+  id serial,
+  hotel_id bigint unsigned not null,
+  text text not null,
+  created_at timestamp not null default current_timestamp,
+  foreign key (hotel_id) references hotel(id)
+);
+
+create table hotel_room_type ( # тип номера
+  id serial,
+  hotel_id bigint unsigned not null,
+  name varchar(255) not null,
+  price int unsigned not null, # цена в день (в рублях)
+  rooms_count int unsigned not null,
+  available_rooms_count int unsigned not null,
+  foreign key (hotel_id) references hotel(id)
+);
+
+create table hotel_room_type_discount ( # процент скидки на тип номера на определённую дату
   room_type_id bigint unsigned not null,
   `date` date not null,
   percentage tinyint unsigned not null,
   primary key (room_type_id, `date`),
-  foreign key (room_type_id) references room_type(id)
+  foreign key (room_type_id) references hotel_room_type(id)
 );
 
 create table room ( # гостиничный номер
   id serial,
   room_type_id bigint unsigned not null,
-  foreign key (room_type_id) references room_type(id)
+  foreign key (room_type_id) references hotel_room_type(id)
 );
 
 create table room_status ( # статус гостиничного номера на конкретную дату
@@ -70,10 +97,12 @@ create table facility ( # удобство
   name varchar(255) not null
 );
 
-create table bed ( # тип кровати
+create table bed ( # спальное место
   id serial,
+  type tinyint unsigned not null, # 1 - взрослое, 2 - детское
   name varchar(255) not null,
-  adults_max_capacity tinyint unsigned not null
+  adults_max_capacity tinyint unsigned not null,
+  kids_max_capacity tinyint unsigned not null
 );
 
 create table photo ( # фото
@@ -81,16 +110,17 @@ create table photo ( # фото
   url varchar(255) not null
 );
 
-create table user ( # пользователь
-  id serial,
-  login varchar(255) not null, # TODO: добавить уникальность поля login
-  hashed_password varchar(255) not null,
-  first_name varchar(255),
-  last_name varchar(255),
-  phone_number varchar(255),
-  email_address varchar(255)
+create table hotel_photo_bind ( # фотография гостиницы
+  hotel_id bigint unsigned not null,
+  photo_id bigint unsigned not null,
+  position tinyint unsigned not null,
+  is_main bool not null default false,
+  foreign key (hotel_id) references hotel(id),
+  foreign key (photo_id) references photo(id),
+  primary key (hotel_id, photo_id)
 );
 
+# TODO: разобраться
 create table payment_status ( # статус оплаты
   id serial,
   status tinyint unsigned,
@@ -98,44 +128,46 @@ create table payment_status ( # статус оплаты
   paid_rubles int unsigned not null default 0
 );
 
-create table `order` ( # заказ
+# TODO: допилить статус оплаты и прочее с оплатой
+create table hotel_order ( # заказ на бронирование номера
   id serial,
-  user_id bigint unsigned not null,
-  room_id bigint unsigned not null,
+  customer_id bigint unsigned not null,
+  room_type_id bigint unsigned not null,
   check_in_date date not null,
   check_out_date date not null,
+  adults_count tinyint unsigned not null,
+  kids_count tinyint unsigned not null default 0,
+  comment varchar(255),
   payment_status_id bigint unsigned not null,
   created_at timestamp not null default current_timestamp,
-  foreign key (user_id) references user(id),
-  foreign key (room_id) references room(id),
+  foreign key (customer_id) references user_customer(id),
+  foreign key (room_type_id) references hotel_room_type(id),
   foreign key (payment_status_id) references payment_status(id)
-  # TODO: добавить пожелания заказчика
-  # TODO: добавить количество взрослых и количество детей
 );
 
+create table hotel_order_room ( # индекс фактического гостиничного номера заказа
+  id serial,
+  order_id bigint unsigned not null,
+  room varchar(255) not null,
+  foreign key (order_id) references hotel_order (id)
+);
+
+# TODO: разобраться
 create table payment_log ( # журнал операций оплат
   id serial,
   order_id bigint unsigned not null,
   amount_in_rubles int unsigned not null,
   created_at timestamp not null default current_timestamp,
-  foreign key (order_id) references `order`(id)
+  foreign key (order_id) references hotel_order (id)
 );
 
 create table payment_type ( # способ оплаты
   id serial,
   name varchar(255)
 );
+insert into payment_type (name) values ('Сбербанк Онлайн'), ('Яндекс.Деньги');
 
-create table hotel_photo_bind ( # фотографии гостиниц
-  hotel_id bigint unsigned not null,
-  photo_id bigint unsigned not null,
-  position tinyint unsigned not null,
-  foreign key (hotel_id) references hotel(id),
-  foreign key (photo_id) references photo(id),
-  primary key (hotel_id, photo_id)
-);
-
-create table hotel_payment_type_bind ( # способы оплаты в гостиницах
+create table hotel_payment_type_bind ( # способ оплаты гостиницы
   hotel_id bigint unsigned not null,
   payment_type_id bigint unsigned not null,
   foreign key (hotel_id) references hotel(id),
@@ -143,19 +175,43 @@ create table hotel_payment_type_bind ( # способы оплаты в гост
   primary key (hotel_id, payment_type_id)
 );
 
-create table room_type_bed_bind ( # типы кроватей для типов гостиничных номеров
+create table hotel_room_type_bed_bind ( # тип кровати у типа номера
   room_type_id bigint unsigned not null,
   bed_id bigint unsigned not null,
-  foreign key (room_type_id) references room_type(id),
+  foreign key (room_type_id) references hotel_room_type(id),
   foreign key (bed_id) references bed(id),
   primary key (room_type_id, bed_id)
 );
 
-create table room_type_facility_bind ( # удобства для типов гостиничных номеров
+create table hotel_room_type_facility_bind ( # удобство у типа номера
   room_type_id bigint unsigned not null,
   facility_id bigint unsigned not null,
-  foreign key (room_type_id) references room_type(id),
+  foreign key (room_type_id) references hotel_room_type(id),
   foreign key (facility_id) references facility(id),
   primary key (room_type_id, facility_id)
 );
 
+create table hotel_review ( # отзыв об отеле
+  id serial,
+  order_id bigint unsigned not null unique,
+  summary_text text not null,
+  advantages_text text not null,
+  disadvantages_text text not null,
+  created_at timestamp not null default current_timestamp,
+  foreign key (order_id) references hotel_order (id)
+);
+
+create table hotel_review_feature ( # характеристка отзыва об отеле
+  id serial,
+  name varchar(255) not null
+);
+insert into hotel_review_feature (name) values ('Удобства'), ('Персонал'), ('Расположение'), ('Чистота'), ('Комфорт');
+
+create table hotel_review_feature_rating ( # оценка характеристики отзыва об отеле
+  id serial,
+  review_id bigint unsigned not null,
+  feature_id bigint unsigned not null,
+  rating tinyint unsigned not null,
+  foreign key (review_id) references hotel_review(id),
+  foreign key (feature_id) references hotel_review_feature(id)
+);
