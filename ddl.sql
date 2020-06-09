84b0a76791a900d0ad4ebdc7913ab3da6cbfb854
Alexander Melnik <colddegree@yandex.ru>
2020-06-10 00:24:11 +1000

    Добавить услуги

--- ddl.sql
+++ ddl.sql
@@ -94,7 +94,7 @@ create table photo ( # фото
   url varchar(255) not null
 );
 
-create table hotel_photo_bind ( # фотография гостиницы
+create table hotel_photo ( # фотография гостиницы
   hotel_id bigint unsigned not null,
   photo_id bigint unsigned not null,
   position tinyint unsigned not null,
@@ -202,6 +202,94 @@ create table complaint ( # жалоба клиента на гостиницу
 );
 
 
-# TODO: услуги + фоточки для услуг
 
 
+create table service_category ( # категория услуг
+  id serial,
+  name varchar(255)
+);
+insert into service_category (id, name) values (1, 'Еда'), (2, 'Напитки'), (3, 'Горячие блюда'), (4, 'Русская кухня');
+
+create table service ( # услуга
+  id serial,
+  type tinyint unsigned not null, # 1 - еда/напитки, 2 - сервис
+  hotel_id bigint unsigned not null,
+  name varchar(255) not null,
+  category_id bigint unsigned not null,
+  has_alcohol bool,
+  nutrition_proteins float unsigned, # пищевая ценность (на 100 г продукта) - белки (в граммах)
+  nutrition_fats float unsigned, # пищевая ценность (на 100 г продукта) - жиры (в граммах)
+  nutrition_carbs float unsigned, # пищевая ценность (на 100 г продукта) - углеводы (в граммах)
+  calories smallint unsigned, # энергетическая ценность (на 100 г продукта) - калории (в ккал)
+  waiting_time smallint unsigned, # время ожидания услуги (в минутах)
+  execution_time smallint unsigned, # время выполнения услуги (в минутах)
+  foreign key (hotel_id) references hotel (id),
+  foreign key (category_id) references service_category (id)
+);
+
+create table service_portion ( # порция услуги (или опция)
+  id serial,
+  service_id bigint unsigned not null,
+  is_option bool not null default false,
+  size int unsigned not null,
+  unit varchar(255) not null, # TODO: может тут nullable?
+  price bigint unsigned not null, # цена порции (в копейках)
+  foreign key (service_id) references service (id)
+);
+
+create table ingredient ( # ингредиент
+  id serial,
+  name varchar(255) not null,
+  hotel_id bigint unsigned, # для того, чтобы можно было менеджеру добавить кастомные ингредиенты (по аналогии с bed)
+  foreign key (hotel_id) references hotel (id)
+);
+
+create table service_ingredient ( # ингредиент услуги
+  service_id bigint unsigned not null,
+  ingredient_id bigint unsigned not null,
+  foreign key (service_id) references service (id),
+  foreign key (ingredient_id) references ingredient (id),
+  primary key (service_id, ingredient_id)
+);
+
+create table service_availability ( # доступность услуги
+  service_id bigint unsigned not null,
+  start smallint unsigned not null, # начало отрезка времени суток (в минутах)
+  end smallint unsigned not null, # конец отрезка времени суток (в минутах)
+  foreign key (service_id) references service (id),
+  primary key (service_id, start, end)
+);
+
+create table service_order ( # заказ услуги
+  id serial,
+  ordered_service_id bigint unsigned not null,
+  order_id bigint unsigned not null,
+  room varchar(255) not null, # индекс гостиничного номера из hotel_order_room по order_id TODO: может быть тут nullable? Ведь не всегда услуга предоставляется в номер. Или всегда?
+  portion_id bigint unsigned not null,
+  portions_amount smallint unsigned not null,
+  customer_comment text,
+  manager_comment text,
+  created_at timestamp not null default current_timestamp,
+  foreign key (ordered_service_id) references service (id),
+  foreign key (order_id) references hotel_order (id),
+  foreign key (portion_id) references service_portion (id)
+);
+
+create table service_order_option ( # опция услуги в заказе услуги
+  order_id bigint unsigned not null,
+  option_id bigint unsigned not null,
+  amount smallint unsigned not null,
+  foreign key (order_id) references service_order (id),
+  foreign key (option_id) references service_portion (id),
+  primary key (order_id, option_id)
+);
+
+create table service_photo ( # фотография услуги
+  service_id bigint unsigned not null,
+  photo_id bigint unsigned not null,
+  position tinyint unsigned not null,
+  is_main bool not null default false,
+  foreign key (service_id) references service (id),
+  foreign key (photo_id) references photo (id),
+  primary key (service_id, photo_id)
+);
